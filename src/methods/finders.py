6ffcdcab4e5c5d6f305450b566b634f988e1ef32
FMCalisto <FMCalisto@users.noreply.github.com>
2019-03-11 16:10:34 +0000

    [ADD] Main: New Conditions

--- src/methods/finders.py
+++ src/methods/finders.py
@@ -41,8 +41,47 @@ sys.path.append(pathAbsPath)
 def find_eyetrackers_meta():
   found_eyetrackers = tr.find_all_eyetrackers()
   available_eyetracker = found_eyetrackers[0]
+
   print("Address: " + available_eyetracker.address)
   print("Model: " + available_eyetracker.model)
   print("Name (It's OK if this is empty): " + available_eyetracker.device_name)
   print("Serial number: " + available_eyetracker.serial_number)
+
+  if tr.CAPABILITY_CAN_SET_DISPLAY_AREA in available_eyetracker.device_capabilities:
+    print("The display area can be set on the eye tracker.")
+  else:
+    print("The display area can not be set on the eye tracker.")
+  if tr.CAPABILITY_HAS_EXTERNAL_SIGNAL in available_eyetracker.device_capabilities:
+    print("The eye tracker can deliver an external signal stream.")
+  else:
+    print("The eye tracker can not deliver an external signal stream.")
+  if tr.CAPABILITY_HAS_EYE_IMAGES in available_eyetracker.device_capabilities:
+    print("The eye tracker can deliver an eye image stream.")
+  else:
+    print("The eye tracker can not deliver an eye image stream.")
+  if tr.CAPABILITY_HAS_GAZE_DATA in available_eyetracker.device_capabilities:
+    print("The eye tracker can deliver a gaze data stream.")
+  else:
+    print("The eye tracker can not deliver a gaze data stream.")
+  if tr.CAPABILITY_HAS_HMD_GAZE_DATA in available_eyetracker.device_capabilities:
+    print("The eye tracker can deliver a HMD gaze data stream.")
+  else:
+    print("The eye tracker can not deliver a HMD gaze data stream.")
+  if tr.CAPABILITY_CAN_DO_SCREEN_BASED_CALIBRATION in available_eyetracker.device_capabilities:
+    print("The eye tracker can do a screen based calibration.")
+  else:
+    print("The eye tracker can not do a screen based calibration.")
+  if tr.CAPABILITY_CAN_DO_MONOCULAR_CALIBRATION in available_eyetracker.device_capabilities:
+    print("The eye tracker can do a monocular calibration.")
+  else:
+    print("The eye tracker can not do a monocular calibration.")
+  if tr.CAPABILITY_CAN_DO_HMD_BASED_CALIBRATION in available_eyetracker.device_capabilities:
+    print("The eye tracker can do a HMD screen based calibration.")
+  else:
+    print("The eye tracker can not do a HMD screen based calibration.")
+  if tr.CAPABILITY_HAS_HMD_LENS_CONFIG in available_eyetracker.device_capabilities:
+    print("The eye tracker can get/set the HMD lens configuration.")
+  else:
+    print("The eye tracker can not get/set the HMD lens configuration.")
+
   return available_eyetracker
